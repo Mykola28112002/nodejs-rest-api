4bfcc029d462889f43a3b19a7a1a9d80b6bdbced
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-11-15 12:27:14 +0200

    1

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const { contactsRouter } = require("./routes/contactsRoute");
+const { userRouter } = require("./routes/userRouter");
 
-const contactsRouter = require('./routes/api/contacts')
+const cors = require("cors");
+const morgan = require("morgan");
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/contacts', contactsRouter)
+app.use("/api/users", userRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  return res.status(404).json({ message: "Not Found" });
+});
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  const { status = 500, message = "Server Internal error" } = err;
+  res.status(status).json({ message });
+});
 
-module.exports = app
+module.exports = {
+  app,
+};
+
+
+
+
