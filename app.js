4dd079ffc5fa15eedaf834d74003d83e7b744d7d
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-11-20 15:22:09 +0200

    1

--- app.js
+++ app.js
@@ -11,10 +11,9 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-
+app.use(express.static("public"));
 app.use("/api/users", userRouter);
 app.use("/api/contacts", contactsRouter);
-app.use("/public", express.static("public"));
 
 app.use((req, res) => {
   return res.status(404).json({ message: "Not Found" });
