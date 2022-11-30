630157edcceabd4771c4898d077bd3aec579b020
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-11-30 13:47:24 +0200

    1

--- Validations/userShema.js
+++ Validations/userShema.js
@@ -2,24 +2,31 @@
 const { Schema, model } = require('mongoose')
 
 const usertSchema = new Schema(
-    {
-    
-    password: {
-        type: String,
-        required: [true, 'Set password for user'],
-    },
-    email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-    },
-    subscription: {
-        type: String,
-        enum: ["starter", "pro", "business"],
-        default: "starter"
+    { 
+        password: {
+            type: String,
+            required: [true, 'Set password for user'],
         },
-    avatarURL: String,
-    token: String,
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            unique: true,
+        },
+        subscription: {
+            type: String,
+            enum: ["starter", "pro", "business"],
+            default: "starter"
+            },
+        avatarURL: String,
+        token: String,
+        verify: {
+            type: Boolean,
+            default: false,
+        },
+        verificationToken: {
+            type: String,
+            required: [true, 'Verify token is required'],
+        }
     },
     {
         timestamps: true,
---
+++ middleweras/nodemailer.js
@@ -0,0 +1,30 @@
+
+const nodemailer = require("nodemailer");
+
+async function sendEmail({email, password, token}) {
+    console.log(email, password, token)
+    const transporter = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+            user: "6a96ef7063c505",
+            pass: "3fcebe90ee5c20"
+        },
+    });
+
+    const url = `http://localhost:3000/api/users/verify/${token}`
+    const emails = {
+        from: 'testing@gmail.com', 
+        to: email, 
+        subject: "Please veryfy your email", 
+        text: 'Please veryfy your email', 
+        html: `<b>Open this link <a href=${url} >${url}</a>,</b>`, 
+    };
+        
+    await transporter.sendMail(emails)
+}
+
+
+module.exports = {
+  sendEmail,
+};
